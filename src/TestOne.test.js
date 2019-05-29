55df748b56ecd47b6d2762d6f9d7539d4ae9bcea
Jason Kinnear <j.g.kinnear@gmail.com>
2019-05-30 09:33:47 +1000

    Updated existing files formatted by prettier

--- src/TestOne.test.js
+++ src/TestOne.test.js
@@ -1,49 +1,49 @@
-import { sortObjectsById } from "./TestOne";
+import { sortObjectsById } from './TestOne';
 
 const HARRY = {
 	id: 2,
-	name: "Harry"
+	name: 'Harry',
 };
 
 const HERMIONE = {
 	id: 1,
-	name: "Hermione"
+	name: 'Hermione',
 };
 const RONALD = {
-	id: "3",
-	name: "Ron"
+	id: '3',
+	name: 'Ron',
 };
 const SIRIUS = {
 	id: 4,
-	name: "Sirius"
+	name: 'Sirius',
 };
 const DOBBY = {
 	id: 4,
-	name: "Dobby"
+	name: 'Dobby',
 };
 
 const TEST_DATA_1 = [HARRY, HERMIONE, RONALD, SIRIUS, DOBBY];
 
-it("should return an array", () => {
+it('should return an array', () => {
 	const result = sortObjectsById(TEST_DATA_1);
 	expect(Array.isArray(result)).toBe(true);
 });
 
-it("should have all items still in the array", () => {
+it('should have all items still in the array', () => {
 	const result = sortObjectsById(TEST_DATA_1);
 	expect(result).toHaveLength(TEST_DATA_1.length);
 });
 
-it("should not mutate the array", () => {
+it('should not mutate the array', () => {
 	const result = sortObjectsById(TEST_DATA_1);
 	expect(result).not.toBe(TEST_DATA_1);
 });
 
-it("should sort the items based on ID", () => {
+it('should sort the items based on ID', () => {
 	const result = sortObjectsById(TEST_DATA_1);
 	expect(result).toEqual([HERMIONE, HARRY, RONALD, DOBBY, SIRIUS]);
 });
 
-it("should throw an error of an array is not provided", () => {
-	expect(() => sortObjectsById("")).toThrow();
+it('should throw an error of an array is not provided', () => {
+	expect(() => sortObjectsById('')).toThrow();
 });
