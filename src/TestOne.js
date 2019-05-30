46aa58ce017bb64c1433450443162b1f2e2e708a
Luke Thompson <lpthompson92@gmail.com>
2019-05-30 17:55:35 +1000

    Completed tests

--- src/TestOne.js
+++ src/TestOne.js
@@ -1,1 +1,11 @@
-export const sortObjectsById = (arrayOfObjects) => {};
+export const sortObjectsById = (arrayOfObjects) => {
+  const clone = arrayOfObjects.slice();
+
+  return clone.sort((a, b) => {
+    if (a.id > b.id) return 1;
+    if (a.id < b.id) return -1;
+
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+  });
+};
--- src/TestTwo.js
+++ src/TestTwo.js
@@ -1,1 +1,12 @@
-export const generateSortingMemoization = (sortByFunction) => {};
+export const generateSortingMemoization = (sortByFunction) => {
+  const cache = new Map();
+
+  return (arg) => {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    } else {
+      cache.set(arg, sortByFunction(arg));
+      return cache.get(arg);
+    }
+  };
+};
